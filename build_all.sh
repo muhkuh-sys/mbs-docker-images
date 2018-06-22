1d259fcb6f68e375955304b477ac675f07427ffe
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-22 09:17:39 +0200

    Set the 64bit CPU ID to x86_64.

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -e
 
-docker build --tag mbs_ubuntu_1204_x64_86 --file dockerfile_mbs_ubuntu_1204_x64_86 .
+docker build --tag mbs_ubuntu_1204_x86_64 --file dockerfile_mbs_ubuntu_1204_x86_64 .
 docker build --tag mbs_ubuntu_1204_x86    --file dockerfile_mbs_ubuntu_1204_x86    .
-docker build --tag mbs_ubuntu_1404_x64_86 --file dockerfile_mbs_ubuntu_1404_x64_86 .
+docker build --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubuntu_1404_x86_64 .
 docker build --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
-docker build --tag mbs_ubuntu_1604_x64_86 --file dockerfile_mbs_ubuntu_1604_x64_86 .
+docker build --tag mbs_ubuntu_1604_x86_64 --file dockerfile_mbs_ubuntu_1604_x86_64 .
 docker build --tag mbs_ubuntu_1604_x86    --file dockerfile_mbs_ubuntu_1604_x86    .
-docker build --tag mbs_ubuntu_1710_x64_86 --file dockerfile_mbs_ubuntu_1710_x64_86 .
+docker build --tag mbs_ubuntu_1710_x86_64 --file dockerfile_mbs_ubuntu_1710_x86_64 .
 docker build --tag mbs_ubuntu_1710_x86    --file dockerfile_mbs_ubuntu_1710_x86    .
-docker build --tag mbs_ubuntu_1804_x64_86 --file dockerfile_mbs_ubuntu_1804_x64_86 .
+docker build --tag mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
 docker build --tag mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
