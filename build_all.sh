b7be7f54ecd4dc67f8ac309ef64ba1304417d99c
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-02-21 14:01:47 +0100

    Disable the cache as it prevents "apt-get update" to run.

--- build_all.sh
+++ build_all.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 set -e
 
-docker build --tag mbs_ubuntu_1204_x86_64 --file dockerfile_mbs_ubuntu_1204_x86_64 .
-docker build --tag mbs_ubuntu_1204_x86    --file dockerfile_mbs_ubuntu_1204_x86    .
-docker build --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubuntu_1404_x86_64 .
-docker build --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
-docker build --tag mbs_ubuntu_1604_x86_64 --file dockerfile_mbs_ubuntu_1604_x86_64 .
-docker build --tag mbs_ubuntu_1604_x86    --file dockerfile_mbs_ubuntu_1604_x86    .
-docker build --tag mbs_ubuntu_1710_x86_64 --file dockerfile_mbs_ubuntu_1710_x86_64 .
-docker build --tag mbs_ubuntu_1710_x86    --file dockerfile_mbs_ubuntu_1710_x86    .
-docker build --tag mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
-docker build --tag mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
+docker build --no-cache --tag mbs_ubuntu_1204_x86_64 --file dockerfile_mbs_ubuntu_1204_x86_64 .
+docker build --no-cache --tag mbs_ubuntu_1204_x86    --file dockerfile_mbs_ubuntu_1204_x86    .
+docker build --no-cache --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubuntu_1404_x86_64 .
+docker build --no-cache --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
+docker build --no-cache --tag mbs_ubuntu_1604_x86_64 --file dockerfile_mbs_ubuntu_1604_x86_64 .
+docker build --no-cache --tag mbs_ubuntu_1604_x86    --file dockerfile_mbs_ubuntu_1604_x86    .
+docker build --no-cache --tag mbs_ubuntu_1710_x86_64 --file dockerfile_mbs_ubuntu_1710_x86_64 .
+docker build --no-cache --tag mbs_ubuntu_1710_x86    --file dockerfile_mbs_ubuntu_1710_x86    .
+docker build --no-cache --tag mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
+docker build --no-cache --tag mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
