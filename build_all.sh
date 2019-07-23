d53a1b2547ddd9ab19a1502aa103731f3ca3cf67
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2019-07-23 16:51:08 +0200

    Remove 17.10 . Add 19.04 .

--- build_all.sh
+++ build_all.sh
@@ -7,9 +7,9 @@ docker build --no-cache --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubunt
 docker build --no-cache --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
 docker build --no-cache --tag mbs_ubuntu_1604_x86_64 --file dockerfile_mbs_ubuntu_1604_x86_64 .
 docker build --no-cache --tag mbs_ubuntu_1604_x86    --file dockerfile_mbs_ubuntu_1604_x86    .
-docker build --no-cache --tag mbs_ubuntu_1710_x86_64 --file dockerfile_mbs_ubuntu_1710_x86_64 .
-docker build --no-cache --tag mbs_ubuntu_1710_x86    --file dockerfile_mbs_ubuntu_1710_x86    .
 docker build --no-cache --tag mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
 docker build --no-cache --tag mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
 docker build --no-cache --tag mbs_ubuntu_1810_x86_64 --file dockerfile_mbs_ubuntu_1810_x86_64 .
 docker build --no-cache --tag mbs_ubuntu_1810_x86    --file dockerfile_mbs_ubuntu_1810_x86    .
+docker build --no-cache --tag mbs_ubuntu_1904_x86_64 --file dockerfile_mbs_ubuntu_1904_x86_64 .
+docker build --no-cache --tag mbs_ubuntu_1904_x86    --file dockerfile_mbs_ubuntu_1904_x86    .
