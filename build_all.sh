aeb1a7bb140119c8b87a14d57eb0ce92484c0dbb
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-03-16 22:04:47 +0100

    Do not build the 12.04 and 14.04 images by default.

--- build_all.sh
+++ build_all.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 set -e
 
-docker build --no-cache --tag mbs_ubuntu_1204_x86_64 --file dockerfile_mbs_ubuntu_1204_x86_64 .
-docker build --no-cache --tag mbs_ubuntu_1204_x86    --file dockerfile_mbs_ubuntu_1204_x86    .
-docker build --no-cache --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubuntu_1404_x86_64 .
-docker build --no-cache --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
+# docker build --no-cache --tag mbs_ubuntu_1204_x86_64 --file dockerfile_mbs_ubuntu_1204_x86_64 .
+# docker build --no-cache --tag mbs_ubuntu_1204_x86    --file dockerfile_mbs_ubuntu_1204_x86    .
+# docker build --no-cache --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubuntu_1404_x86_64 .
+# docker build --no-cache --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
 docker build --no-cache --tag mbs_ubuntu_1604_x86_64 --file dockerfile_mbs_ubuntu_1604_x86_64 .
 docker build --no-cache --tag mbs_ubuntu_1604_x86    --file dockerfile_mbs_ubuntu_1604_x86    .
 docker build --no-cache --tag mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
