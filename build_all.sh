2c3701de153cdb545ece1efc484b6398f154f7aa
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2022-03-31 14:42:52 +0200

    Drop i386 builds with Ubuntu 22.04 .

--- build_all.sh
+++ build_all.sh
@@ -18,4 +18,3 @@ docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1804_x86    --file d
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2004_x86_64 --file dockerfile_mbs_ubuntu_2004_x86_64 .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2004_x86    --file dockerfile_mbs_ubuntu_2004_x86    .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2204_x86_64 --file dockerfile_mbs_ubuntu_2204_x86_64 .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2204_x86    --file dockerfile_mbs_ubuntu_2204_x86    .
