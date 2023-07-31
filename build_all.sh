9220e0e646634be69e554aaa79f0435a9f071be9
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-07-31 15:45:41 +0200

    Delete old images.

--- build_all.sh
+++ build_all.sh
@@ -1,20 +1,8 @@
 #! /bin/bash
 set -e
 
-# docker build --no-cache --tag mbs_ubuntu_1204_x86_64 --file dockerfile_mbs_ubuntu_1204_x86_64 .
-# docker build --no-cache --tag mbs_ubuntu_1204_x86    --file dockerfile_mbs_ubuntu_1204_x86    .
-# docker build --no-cache --tag mbs_ubuntu_1404_x86_64 --file dockerfile_mbs_ubuntu_1404_x86_64 .
-# docker build --no-cache --tag mbs_ubuntu_1404_x86    --file dockerfile_mbs_ubuntu_1404_x86    .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1604_x86_64 --file dockerfile_mbs_ubuntu_1604_x86_64 .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1604_x86    --file dockerfile_mbs_ubuntu_1604_x86    .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
-# docker build --no-cache --tag mbs_ubuntu_1810_x86_64 --file dockerfile_mbs_ubuntu_1810_x86_64 .
-# docker build --no-cache --tag mbs_ubuntu_1810_x86    --file dockerfile_mbs_ubuntu_1810_x86    .
-# docker build --no-cache --tag mbs_ubuntu_1904_x86_64 --file dockerfile_mbs_ubuntu_1904_x86_64 .
-# docker build --no-cache --tag mbs_ubuntu_1904_x86    --file dockerfile_mbs_ubuntu_1904_x86    .
-# docker build --no-cache --tag mbs_ubuntu_1910_x86_64 --file dockerfile_mbs_ubuntu_1910_x86_64 .
-# docker build --no-cache --tag mbs_ubuntu_1910_x86    --file dockerfile_mbs_ubuntu_1910_x86    .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2004_x86_64 --file dockerfile_mbs_ubuntu_2004_x86_64 .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2004_x86    --file dockerfile_mbs_ubuntu_2004_x86    .
 docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2204_x86_64 --file dockerfile_mbs_ubuntu_2204_x86_64 .
