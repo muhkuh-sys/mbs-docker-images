1da298d590797c71ac44ef142fd0e9274bcd4948
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-07-31 15:46:34 +0200

    Move the cache control to a variable.

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 set -e
+CACHECFG=--no-cache
 
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2004_x86_64 --file dockerfile_mbs_ubuntu_2004_x86_64 .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2004_x86    --file dockerfile_mbs_ubuntu_2004_x86    .
-docker build --no-cache --tag ghcr.io/docbacardi/mbs_ubuntu_2204_x86_64 --file dockerfile_mbs_ubuntu_2204_x86_64 .
+docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_1804_x86_64 --file dockerfile_mbs_ubuntu_1804_x86_64 .
+docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_1804_x86    --file dockerfile_mbs_ubuntu_1804_x86    .
+docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2004_x86_64 --file dockerfile_mbs_ubuntu_2004_x86_64 .
+docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2004_x86    --file dockerfile_mbs_ubuntu_2004_x86    .
+docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2204_x86_64 --file dockerfile_mbs_ubuntu_2204_x86_64 .
