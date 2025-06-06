95644c133c82b1a926c5b983fa5ca3e66086cdca
bedudu98 <dungdungtran98@gmail.com>
2025-06-06 15:00:50 +0200

    Install m4

--- build_all.sh
+++ build_all.sh
@@ -5,6 +5,8 @@ CACHECFG=--no-cache
 docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2004_x86_64 --file dockerfile_mbs_ubuntu_2004_x86_64 .
 docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2004_x86    --file dockerfile_mbs_ubuntu_2004_x86    .
 docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2204_x86_64 --file dockerfile_mbs_ubuntu_2204_x86_64 .
+docker build ${CACHECFG} --platform linux/riscv64 --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2204_riscv64 --file dockerfile_mbs_ubuntu_2204_riscv64 .
+docker build ${CACHECFG} --platform linux/arm64 --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2204_arm64 --file dockerfile_mbs_ubuntu_2204_arm64 .
 docker build ${CACHECFG} --platform linux/arm64 --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2404_arm64 --file dockerfile_mbs_ubuntu_2404_arm64 .
 docker build ${CACHECFG} --platform linux/riscv64 --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2404_riscv64 --file dockerfile_mbs_ubuntu_2404_riscv64 .
 docker build ${CACHECFG} --tag ghcr.io/muhkuh-sys/mbs_ubuntu_2404_x86_64 --file dockerfile_mbs_ubuntu_2404_x86_64 .
